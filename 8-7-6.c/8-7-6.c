9cadccc8d5aaa2cab7f844c5cffeb5cd07f91902
Your Name <you@example.com>
2021-09-11 02:28:09 +0800

    添加项目文件。

---
+++ 8-7-6.c/8-7-6.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_DEPRECATE 1
+#include<stdio.h>
+//#include"add.h"
+#include"123.h"
+int main()
+{
+	int k = 10;
+	int b = 90;
+	int sum = 0;
+	scanf("%d %d", &k, &b);
+	sum = add(k, b);
+	printf("ºÍ=%d", sum);
+
+
+	return 0;
+}
+
---
+++ 8-7-6.c/add.h
@@ -0,0 +1,2 @@
+#define _CRT_SECURE_NO_DEPRECATE 1
+int add(int, int);
